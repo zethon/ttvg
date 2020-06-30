94c09c3faa2f35046ee79183b725f4946feed3ce
Max Power <maxpower@anothermessageboard.com>
2020-06-30 11:12:48 -0400

    before resize;

---
+++ resources/maps/houses2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="houses2" tilewidth="16" tileheight="16" tilecount="40" columns="10">
+ <image source="assets/houses2.png" trans="ff00ff" width="160" height="64"/>
+</tileset>
