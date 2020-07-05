adfb9e09a4b67cb1926567cd8fb028dd9d978e33
Max Power <maxpower@anothermessageboard.com>
2020-07-05 09:44:10 -0400

    asset update;

---
+++ resources/src/maps/buildings1.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="buildings1" tilewidth="16" tileheight="16" tilecount="3025" columns="55">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="assets/buildings1.png" trans="000000" width="894" height="894"/>
+</tileset>
