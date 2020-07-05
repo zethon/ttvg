d8be24f4d75dd2a786ae996e3f54ae6ab2052d58
Max Power <maxpower@anothermessageboard.com>
2020-07-05 16:40:24 -0400

    new building assets

---
+++ resources/src/maps/buildings2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="buildings2" tilewidth="16" tileheight="16" tilecount="2775" columns="75">
+ <image source="assets/buildings2.png" width="1200" height="600"/>
+</tileset>
