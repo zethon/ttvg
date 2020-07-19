e591d811f1fa2969436c002d097be5498c3072eb
Max Power <maxpower@anothermessageboard.com>
2020-07-19 11:08:29 -0400

    adding test harness for `Backgroun` class;

---
+++ tests/test_background.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+
+#include <SFML/Graphics.hpp>
+
+BOOST_AUTO_TEST_SUITE(tt)
+BOOST_AUTO_TEST_SUITE(background)
+
+BOOST_AUTO_TEST_CASE(xyz)
+{
+    
+}
+
+BOOST_AUTO_TEST_SUITE_END() // background
+BOOST_AUTO_TEST_SUITE_END() // tt
