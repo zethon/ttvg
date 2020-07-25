5166a4537a24a7b14fe2be689ce143cd4b990d2c
Max Power <maxpower@anothermessageboard.com>
2020-07-25 13:11:47 -0400

    Texture caching in ResourceManager;

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -8,6 +8,33 @@ ResourceManager::ResourceManager(const boost::filesystem::path& path)
 {
 }
 
+////////////////////////////////////////////////////////////
+bool ResourceManager::cacheTexture(const std::string& name)
+{
+    const auto filename = getFilename(name);
+
+    if (sf::Texture texture;
+        texture.loadFromFile(filename))
+    {
+        _textcache.emplace(name, std::move(texture));
+        return true;
+    }
+
+    return false;
+}
+
+////////////////////////////////////////////////////////////
+sf::Texture* ResourceManager::getTexture(const std::string& name)
+{
+    if (_textcache.find(name) != _textcache.end())
+    {
+        return &(_textcache[name]);
+    }
+
+    return nullptr;
+}
+
+////////////////////////////////////////////////////////////
 std::string ResourceManager::getFilename(const std::string& name)
 {
     auto filepath = _resourceFolder / name;
