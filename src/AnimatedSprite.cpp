b53c8d004bec34d879fd756dc468af64c98058a1
Max Power <maxpower@anothermessageboard.com>
2020-07-27 20:54:05 -0400

    it's a bounding box.. of some type;

--- src/AnimatedSprite.cpp
+++ src/AnimatedSprite.cpp
@@ -5,18 +5,27 @@
 namespace tt
 {
 
+constexpr auto HIGHLIGHT_WIDTH = 32;
+constexpr auto HIGHLIGHT_HEIGHT = 32;
+
 AnimatedSprite::AnimatedSprite(const sf::Texture& texture, const sf::Vector2i& size)
     :   _size{ size }
 {
-    setTexture(texture);
-    setTextureRect(sf::IntRect(0, 0, _size.x, _size.y));
+    _sprite.setTexture(texture);
+    _sprite.setTextureRect(sf::IntRect(0, 0, _size.x, _size.y));
+
+    _highlight.setFillColor(sf::Color::Transparent);
+    _highlight.setOutlineThickness(2);
+    _highlight.setOutlineColor(sf::Color(255, 255, 255));
+    _highlight.setSize(sf::Vector2f{ size });
+    _highlight.setPosition(20, 20);
 }
 
 void AnimatedSprite::setSource(std::uint32_t x, std::uint32_t y)
 {
     _source.x = x;
     _source.y = y;
-    setTextureRect(sf::IntRect(
+    _sprite.setTextureRect(sf::IntRect(
         _source.x * _size.x, _source.y * _size.y, _size.x, _size.y));
 }
 
@@ -45,7 +54,7 @@ std::uint16_t AnimatedSprite::timestep()
         left++;
 
         auto textureWidth = _maxFramesPerRow > 0 ?
-            _maxFramesPerRow * _size.x : getTexture()->getSize().x;
+            _maxFramesPerRow * _size.x : _sprite.getTexture()->getSize().x;
 
         if (textureWidth <= static_cast<std::uint32_t>(left * _size.x))
         {
@@ -63,6 +72,26 @@ std::uint16_t AnimatedSprite::timestep()
     }
 
     return 0;
+}
+
+void AnimatedSprite::setHighlight(bool h)
+{
+    if (h)
+    {
+        _highlight.setSize(sf::Vector2f(HIGHLIGHT_WIDTH, HIGHLIGHT_WIDTH));
+    }
+    else
+    {
+        _highlight.setSize(sf::Vector2f{ 0.f, 0.f });
+    }
+
+}
+
+void AnimatedSprite::draw(sf::RenderTarget & target, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+    target.draw(_sprite, states);
+    target.draw(_highlight, states);
 }
 
 } // namespace tt
--- src/AnimatedSprite.h
+++ src/AnimatedSprite.h
@@ -18,7 +18,8 @@ using AnimatedSpritePtr = std::shared_ptr<AnimatedSprite>;
 using AnimeCallback = std::function<void()>;
 
 class AnimatedSprite :
-    public sf::Sprite,
+    public sf::Drawable,
+    public sf::Transformable,
     public IUpdateable
 {
 
@@ -37,7 +38,18 @@ public:
     Direction direction() const { return _direction; }
     void setDirection(Direction val) { _direction = val; }
 
+    sf::FloatRect getGlobalBounds() const
+    {
+        const auto textureRect = _sprite.getTextureRect();
+        float width = static_cast<float>(std::abs(textureRect.width));
+        float height = static_cast<float>(std::abs(textureRect.height));
+        return getTransform().transformRect(sf::FloatRect(0.f, 0.f, width, height));
+    }
+
+    void setHighlight(bool h);
+
 protected:
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override final;
 
     AnimatedState   _state = AnimatedState::STILL;
     Direction       _direction = Direction::DOWN;
@@ -51,6 +63,9 @@ protected:
     std::uint32_t   _maxFramesPerRow = 0;
 
     AnimeCallback   _animeCallback;
+
+    sf::Sprite          _sprite;
+    sf::RectangleShape  _highlight;
 };
 
 } // namespace tt
