2efaccde7f6dd8620eba479086f58e4d7ca763c6
Max Power <maxpower@anothermessageboard.com>
2020-08-06 18:46:04 -0400

    remodeling Tommy's house;

---
+++ resources/src/maps/interior3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="interior3" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
+ <image source="assets/interior3.png" width="768" height="768"/>
+</tileset>
