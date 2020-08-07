2bf616469dd2df16f764e6922650d4e1dca1871e
Max Power <maxpower@anothermessageboard.com>
2020-08-07 19:45:24 -0400

    more housekeeping

--- src/TransitionPoint.cpp
+++ src/TransitionPoint.cpp
@@ -7,8 +7,6 @@ namespace tt
 
 void from_json(const nl::json& j, Transition& t)
 {
-    //j.at("id").get_to(t.id);
-    //j.at("description").get_to(t.description);
     j.at("enabled").get_to(t.enabled);
     if (j.at("type").get<std::string>() == "manual")
     {
@@ -19,7 +17,6 @@ void from_json(const nl::json& j, Transition& t)
         t.type = Transition::Type::AUTO;
     }
 
-    //t.position = sf::Vector2f{ j.at("x").get<float>(), j.at("y").get<float>() };
     j.at("newscene").get_to(t.newscene);
 }
 
