e511bfc21742fadd46e635d82f5b52b4975369cb
Max Power <maxpower@anothermessageboard.com>
2020-08-07 18:19:16 -0400

    chinese restaurant

--- resources/src/maps/chinese-restaurant.tsx
+++ resources/src/maps/chinese-restaurant.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="chinese-restaurant" tilewidth="16" tileheight="16" tilecount="176" columns="16">
- <image source="assets/chinese-restaurant.png" width="269" height="178"/>
+<tileset version="1.4" tiledversion="1.4.1" name="chinese-restaurant" tilewidth="16" tileheight="16" tilecount="60" columns="10">
+ <image source="assets/chinese-restaurant.png" width="160" height="103"/>
 </tileset>
