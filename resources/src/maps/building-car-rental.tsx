9ab2c64c0fedc64fb40e3d199a39e612118d27e1
charmyt <chairmontoad@gmail.com>
2020-08-16 06:12:54 -0700

    Add car rental to map. Tiled tool sucks.

---
+++ resources/src/maps/building-car-rental.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="building-car-rental" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="assets/buildings-car-rental.png" trans="ff00ff" width="256" height="256"/>
+</tileset>
---
+++ resources/src/maps/buildings-car-rental.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="buildings-car-rental" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="assets/buildings-car-rental.png" trans="ff00ff" width="192" height="192"/>
+</tileset>
