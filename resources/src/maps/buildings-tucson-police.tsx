5e5741de8d347ef3a87750bc8421408596702cad
charmyt <chairmontoad@gmail.com>
2020-08-17 17:25:59 -0700

    Add police station and courthouse.

---
+++ resources/src/maps/buildings-tucson-police.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="buildings-tucson-police" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="assets/buildings-tucson-police.png" trans="ff00ff" width="192" height="178"/>
+</tileset>
