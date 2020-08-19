f20b0387c36879fe3da4b6d0776bfad90cad2879
charmyt <chairmontoad@gmail.com>
2020-08-18 22:53:48 -0700

    Add hospital to map.

---
+++ resources/src/maps/buildings-hospital.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="buildings-hospital" tilewidth="16" tileheight="16" tilecount="110" columns="11">
+ <image source="assets/buildings-hospital.png" trans="ff00ff" width="179" height="168"/>
+</tileset>
