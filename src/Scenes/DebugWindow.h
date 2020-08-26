2490f01895b49a62589f2f9152bee159f412e424
Max Power <maxpower@anothermessageboard.com>
2020-08-25 21:05:39 -0400

    moved debug window code to cpp file;

--- src/Scenes/DebugWindow.h
+++ src/Scenes/DebugWindow.h
@@ -13,38 +13,9 @@ class DebugWindow : public Screen
     std::shared_ptr<sf::Text>           _debugText;
 
 public:
-    DebugWindow(ResourceManager& resmgr, sf::RenderTarget& target)
-        : Screen(resmgr, target)
-    {
-        setVisible(false);
+    DebugWindow(ResourceManager& resmgr, sf::RenderTarget& target);
 
-        if (auto temp = _resources.load<sf::Font>("fonts/mono_bold.ttf");
-                !temp.has_value())
-        {
-            throw std::runtime_error("could not load resource 'fonts/mono_bold.ttf'");
-        }
-        else
-        {
-            _debugFont = *(temp);
-        }
-
-        _background = std::make_shared<sf::RectangleShape>();
-        _background->setFillColor(sf::Color{ 0, 0, 0, 104 });
-        _background->setPosition(0.f, _window.getSize().y - 40.f);
-        _background->setSize(sf::Vector2f{ static_cast<float>(_window.getSize().x), 40.f });
-        addDrawable(_background);
-
-        _debugText = std::make_shared<sf::Text>("", _debugFont);
-        _debugText->setFillColor(sf::Color::White);
-        _debugText->setPosition(10.f, _window.getSize().y - 35.f);
-        _debugText->setCharacterSize(20);
-        addDrawable(_debugText);
-    }
-
-    void setText(const std::string& text)
-    {
-        _debugText->setString(text);
-    }
+    void setText(const std::string& text);
 };
 
 }  // namespace tt
