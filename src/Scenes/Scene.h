cda2610c3e3f8d5f1181df19a8cd68a060c7452a
Max Power <maxpower@anothermessageboard.com>
2020-08-30 17:24:48 -0400

    friendlied lua test;

--- src/Scenes/Scene.h
+++ src/Scenes/Scene.h
@@ -31,7 +31,59 @@ using ScenePtr = std::unique_ptr<Scene>;
 using SceneSharedPtr = std::shared_ptr<Scene>;
 
 void from_json(const nl::json& j, AvatarInfo& av);
-int loadSceneLuaFile(Scene& scene, const std::string& filename, lua_State* L);
+
+template<typename SceneT>
+int loadSceneLuaFile(SceneT& scene, const std::string& filename, lua_State* L)
+{
+    if (!L) return 0;
+
+    // load the Scene's Lua file into its own sandboxed
+    // environment which also contains everything in _G
+    {
+        lua_newtable(L); // 1:tbl
+        if (luaL_loadfile(L, filename.c_str()) != 0) // 1:tbl, 2:chunk
+        {
+            auto error = lua_tostring(L, -1);
+            throw std::runtime_error(error);
+        }
+
+        lua_newtable(L); // 1:tbl, 2:chunk, 3:tbl(mt)
+        lua_getglobal(L, "_G"); // 1:tbl, 2:chunk, 3:tbl(mt), 4:_G
+        lua_setfield(L, 3, "__index"); // 1:tbl, 2:chunk, 3:tbl(mt)
+        lua_setmetatable(L, 1); // 1:tbl, 2:chunk
+        lua_pushvalue(L, 1); // 1:tbl, 2:chunk, 3:tbl
+
+        lua_setupvalue(L, -2, 1); // 1:tbl, 2:chunk
+        if (lua_pcall(L, 0, 0, 0) != 0) // 1:tbl
+        {
+            auto error = lua_tostring(L, -1);
+            throw std::runtime_error(error);
+        }
+
+        lua_setglobal(L, scene.name().c_str()); // empty stack
+        assert(lua_gettop(L) == 0);
+    }
+
+    // create a pointer to `this` in the Lua state and register
+    // it as a `Scene` class/object/table inside Lua
+    {
+        // create the pointer to ourselves in the Lua state
+        std::size_t size = sizeof(SceneT*);
+        SceneT** data = static_cast<SceneT**>(lua_newuserdata(L, size)); // -1:ud
+        *data = &scene;
+
+        // and set the metatable
+        luaL_getmetatable(L, SceneT::CLASS_NAME); // -2:ud, -1: mt
+        lua_setmetatable(L, -2); // -1: ud
+        auto idx = luaL_ref(L, LUA_REGISTRYINDEX);  // empty stack
+
+        // make sure we're balanced
+        assert(lua_gettop(L) == 0);
+        return idx;
+    }
+
+    return 0;
+}
 
 class Scene : public Screen
 {
--- tests/test_lua.cpp
+++ tests/test_lua.cpp
@@ -17,21 +17,92 @@ namespace data = boost::unit_test::data;
 BOOST_AUTO_TEST_SUITE(tt)
 BOOST_AUTO_TEST_SUITE(lua)
 
-// --run_test=tt/lua/loadTest
-BOOST_AUTO_TEST_CASE(loadTest)
+//// --run_test=tt/lua/loadTest
+//BOOST_AUTO_TEST_CASE(loadTest)
+//{
+//    tt::NullWindow window;
+//    const auto resfolder = fmt::format("{}/resources", TT_SRC_DIRECTORY_);
+//    tt::ResourceManager resources{ boost::filesystem::path { resfolder } };
+//
+//    tt::GameScreen screen{ resources, window };
+//    auto lua = screen.lua();
+//
+//    BOOST_TEST(lua != nullptr);
+//    for (const auto&[name, scene] : screen.scenes())
+//    {
+//
+//    }
+//}
+
+class GameScreenStub{};
+
+struct SceneStub
 {
-    tt::NullWindow window;
-    const auto resfolder = fmt::format("{}/resources", TT_SRC_DIRECTORY_);
-    tt::ResourceManager resources{ boost::filesystem::path { resfolder } };
+    static constexpr auto CLASS_NAME = "Scene";
+    std::string _name;
+    std::string name() const { return _name; }
+};
 
-    tt::GameScreen screen{ resources, window };
-    auto lua = screen.lua();
+constexpr auto file1 = R"lua(
+function onInit()
+return "mylua1"
+end
+)lua";
 
-    BOOST_TEST(lua != nullptr);
-    for (const auto&[name, scene] : screen.scenes())
+constexpr auto file2 = R"lua(
+function onInit()
+return "mylua2"
+end
+)lua";
+
+
+std::string callFunction(const std::string& funcname, lua_State* L, const std::string& envname, int objIdx)
+{
+    // first get the execution environment and set that
+    lua_getglobal(L, envname.c_str()); // 1:env
+    BOOST_TEST((lua_isnil(L, 1) == 0));
+
+    // now load up the init function
+    lua_getfield(L, 1, funcname.c_str()); // 1:env, 2:func
+    BOOST_TEST((lua_isnil(L, 2) == 0));
+    BOOST_TEST((lua_isfunction(L, 2) == 1));
+
+    // now get the parameter we're passing to Lua which is a Scene* (aka `this`)
+    lua_rawgeti(L, LUA_REGISTRYINDEX, objIdx); // 1:env, 2:func, 1:ud
+    if (lua_pcall(L, 1, 1, 0) != 0) // 1:env, 2:retval
     {
-
+        auto error = lua_tostring(L, -1);
+        BOOST_TEST(false, error);
     }
+
+    std::string retval = lua_tostring(L, -1);
+    lua_settop(L, 0);
+    return retval;
+}
+
+BOOST_AUTO_TEST_CASE(loadTestCase)
+{
+    GameScreenStub stub;
+    lua_State* lua = luaL_newstate();
+
+    tt::initLua(lua, stub);
+    BOOST_TEST_REQUIRE(lua != nullptr);
+
+    auto path = tt::tempFolder();
+    const auto luafile1 = (path / "file1.lua").string();
+    const auto luafile2 = (path / "file2.lua").string();
+
+    writeFile(luafile1, file1);
+    writeFile(luafile2, file2);
+
+    SceneStub s1{ "scene1" };
+    SceneStub s2{ "scene2" };
+
+    auto s1idx = tt::loadSceneLuaFile(s1, luafile1, lua);
+    auto s2idx = tt::loadSceneLuaFile(s2, luafile2, lua);
+
+    BOOST_TEST(callFunction("onInit", lua, "scene1", s1idx) == "mylua1");
+    BOOST_TEST(callFunction("onInit", lua, "scene2", s2idx) == "mylua2");
 }
 
 BOOST_AUTO_TEST_SUITE_END() // lua
