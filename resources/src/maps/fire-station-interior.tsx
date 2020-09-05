f9f369a30d7fb9872cb3ea1501750880f0b23a69
charmyt <chairmontoad@gmail.com>
2020-09-05 05:40:55 -0700

    Improve fire station interior.

--- resources/src/maps/fire-station-interior.tsx
+++ resources/src/maps/fire-station-interior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="fire-station-interior" tilewidth="16" tileheight="16" tilecount="1250" columns="50">
- <image source="assets/fire-station-interior.png" trans="ff00ff" width="800" height="400"/>
+<tileset version="1.4" tiledversion="1.4.1" name="fire-station-interior" tilewidth="16" tileheight="16" tilecount="2500" columns="50">
+ <image source="assets/fire-station-interior.png" trans="ff00ff" width="800" height="800"/>
 </tileset>
