517504c0cde2528e64a81c3e0517ac9be62ceddb
Max Power <maxpower@anothermessageboard.com>
2020-09-08 09:55:32 -0400

    updating Zone.onSelect docs;

--- resources/lua/tucson.lua
+++ resources/lua/tucson.lua
@@ -1,16 +1,20 @@
 local gDT = nil
+local gPlayer = nil
 
 function onInit(scene)
     print("onInit call for '" .. scene:name() .. "'")
     gDT = scene:getDescriptionWindow()
 end
 
+function onEnter(scene)
+    gPlayer = scene:getPlayer()
+end 
+
 function treasureChest_onPickup(scene, item)
-    local player = scene:getPlayer()
-    if player:hasItemByName("key") then
+    if gPlayer:hasItemByName("key") then
         local vagina = ItemFactory.createItem("magic-space-vagina")
-        player:addItem(vagina)
-        player:removeItemByName("key")
+        gPlayer:addItem(vagina)
+        gPlayer:removeItemByName("key")
         scene:removeItem(item)
         gDT:setText("You opened the chest!")
     else
