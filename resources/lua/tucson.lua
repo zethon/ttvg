c3b39be1319429569ed777f196a1a912b700cc4f
Addy <zethon@users.noreply.github.com>
2020-09-15 20:00:44 -0400

    Added a simple means to make money; (#73)

--- resources/lua/tucson.lua
+++ resources/lua/tucson.lua
@@ -25,3 +25,25 @@ function treasureChest_onPickup(scene, item)
 
     w:exec()
 end
+
+local firstTimeSevan = true
+function sevan_select(scene, item)
+    if firstTimeSevan then
+        local w = scene:createModal(ModalType.Messages)
+        w:pushMessage("Oh hi Tommy! How does your hobohole feel?")
+        w:pushMessage("I know I was little rough on you\n that night\nLOL")
+        w:pushMessage("Maybe you can help me. Do you know where I can\nfind some weed?")
+        w:pushMessage("If you bring me a bag weed I'll pay you $20")
+        w:pushMessage("And if you're feeling horny, let me know!\nI miss that dirty hole!")
+        w:exec()
+        firstTimeSevan = false
+    else
+        if not (gPlayer:hasItemByName("bag-of-weed")) then
+            Utils.showModal(scene, "Come back when you have some weed.")
+        else
+            Utils.showModal(scene, "Hey thanks man! Here's another $20")
+            gPlayer:removeItemByName('bag-of-weed')
+            gPlayer:setBalance(gPlayer:getBalance() + 20)
+        end
+    end
+end
