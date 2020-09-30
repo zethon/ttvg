85d3160df450dc4067fc56184ff8aabe8d390afe
Max Power <maxpower@anothermessageboard.com>
2020-09-29 22:28:26 -0400

    meh

--- src/Entity.h
+++ src/Entity.h
@@ -1,9 +1,25 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
+#include "GameTypes.h"
+#include "IUpdateable.h"
+
 namespace tt
 {
 
-class Entity
+class EntityState
+{
+    std::string     id;
+    sf::Vector2i    source;
+    std::uint32_t   frameCount = 1;
+};
+
+
+class Entity : 
+    public sf::Drawable,
+    public sf::Transformable,
+    public IUpdateable
 {
 
 };
