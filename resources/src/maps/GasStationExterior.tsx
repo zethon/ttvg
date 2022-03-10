eba742deafe96dce2273fdb89704c172c6efc0cd
charmyt <chairmontoad@gmail.com>
2022-03-09 22:24:06 -0800

    Add gas stations.

---
+++ resources/src/maps/GasStationExterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="GasStationExterior" tilewidth="16" tileheight="16" tilecount="60" columns="10">
+ <image source="assets/gas-station-exterior.png" trans="ff00ff" width="160" height="96"/>
+</tileset>
