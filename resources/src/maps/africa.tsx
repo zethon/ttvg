b2533eb42eac411bde9c1b39502c7197834785c0
charmyt <chairmontoad@gmail.com>
2022-03-27 13:44:57 -0700

    Add africa.

---
+++ resources/src/maps/africa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="africa" tilewidth="16" tileheight="16" tilecount="19460" columns="139">
+ <image source="assets/africa.png" trans="ff00ff" width="2229" height="2247"/>
+</tileset>
