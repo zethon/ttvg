9ca6e5985a61c8e96a57a76fd7a1601a9c67b2cb
charmyt <chairmontoad@gmail.com>
2022-03-26 17:20:07 -0700

    Add airport.

---
+++ resources/src/maps/tucson-airport-exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Tucson Airport" tilewidth="16" tileheight="16" tilecount="1053" columns="39">
+ <image source="assets/tucson-airport.png" trans="ff00ff" width="626" height="447"/>
+</tileset>
