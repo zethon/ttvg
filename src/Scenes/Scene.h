dcec3d051f33dfa9870a1a4a15debca23ed32d4c
charmyt <chairmontoad@gmail.com>
2022-04-13 23:03:24 -0700

    Fix default values in .h file.

--- src/Scenes/Scene.h
+++ src/Scenes/Scene.h
@@ -132,9 +132,10 @@ int Scene_getDescriptionWindow(lua_State* L);
 struct BackgroundMusic
 {
     std::string file;
-    float volume;
-    bool loop;
-    bool enabled;
+
+    float volume    = 100.0f;
+    bool loop       = false;
+    bool enabled    = false;
 };
 
 void from_json(const nl::json& j, BackgroundMusic& bm);
